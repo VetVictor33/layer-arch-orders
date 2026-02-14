0e888ba4c762c3fd5fd47525f2fefa2b018b5bac
Victor Feliciano <115307935+VetVictor33@users.noreply.github.com>
2026-02-14 15:30:54 -0300

    refactor(health): remove unused route options for health checks

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -1,32 +1,14 @@
-import type { FastifyInstance, RouteShorthandOptions } from "fastify";
-
-const healthCheckOptions: RouteShorthandOptions = {
-  schema: {
-    response: {
-      200: {
-        type: "object",
-        properties: {
-          status: {
-            type: "string",
-          },
-          timestamp: {
-            type: "string",
-          },
-        },
-      },
-    },
-  },
-};
+import type { FastifyInstance } from "fastify";
 
 export const healthRoutes = [
   async (server: FastifyInstance) => {
-    server.get("/ping", healthCheckOptions, async (request, reply) => {
+    server.get("/ping", async (request, reply) => {
       return { pong: "it worked!" };
     });
   },
 
   async (server: FastifyInstance) => {
-    server.get("/health", healthCheckOptions, async (request, reply) => {
+    server.get("/health", async (request, reply) => {
       return {
         status: "ok",
         timestamp: new Date().toISOString(),
