d04cd51d9ff183f099173049511770fe42ee5cae
Victor Feliciano <115307935+VetVictor33@users.noreply.github.com>
2026-02-16 16:48:17 -0300

    feat(order): refactor payment processing to use QueueManager and improve data handling

--- src/services/order-processor.ts
+++ src/services/order-processor.ts
@@ -1,13 +1,17 @@
 import { OrderRepository } from "@/repositories/OrderRepository.js";
 import Service from "@/services/service.js";
 import type { OrderInput } from "@/global/schemas/orders.js";
-import PaymentGatewayMock from "@/services/payment-gateway-mock.js";
+import PaymentGatewayMock, {
+  type PaymentRequest,
+} from "@/services/payment-gateway-mock.js";
+import QueueManager from "@/libs/bullmq.js";
+import { logger } from "@/libs/logger.js";
 
 export default class OrderProcessorService extends Service {
   public async execute(input: OrderInput) {
     const repo = new OrderRepository();
 
-    const creationResponde = await repo.create({
+    const order = await repo.create({
       customerEmail: input.customer.email,
       customerName: input.customer.name,
       paymentStatus: "PENDING",
@@ -16,18 +20,20 @@ export default class OrderProcessorService extends Service {
       productId: input.product.id,
     });
 
-    const paymentService = new PaymentGatewayMock();
+    const queueManager = QueueManager.getInstance();
 
-    const payment = await paymentService.processPayment({
-      orderId: creationResponde.id,
+    // TO-DO: critical -> this saves sensitive data to our DB (card data).
+    // Implement card tokenization to avoid it
+    await queueManager.addJob<PaymentRequest>("payment-processing", {
+      orderId: order.id,
       customerName: input.customer.name,
       customerEmail: input.customer.email,
       amount: input.product.price,
       card: input.payment.card,
     });
 
-    return {
-      status: payment.status,
-    };
+    logger.info(`Payment for order ${order.id} queued.`);
+
+    return { orderId: order.id, status: order.paymentStatus };
   }
 }
